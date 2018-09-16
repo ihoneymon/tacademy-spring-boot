733b26b0acd52de165cb922fc3aa77cb8e99d9a8
honeymon <ihoneymon@gmail.com>
2018-09-16 14:44:51 +0900

    `web` 모듈 제거 및 `Book` 생성자 변경
    * `web` 모듈은 나중으로 미루고...
    * `Book` 필드 isbn -> isbn13, isbn10 변경

--- api/src/integration-test/java/io/honeymon/springboot/t/bookstore/api/controller/BookControllerTest.java
+++ api/src/integration-test/java/io/honeymon/springboot/t/bookstore/api/controller/BookControllerTest.java
@@ -4,10 +4,12 @@ import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import io.honeymon.springboot.t.bookstore.api.service.book.BookService;
-import io.honeymon.springboot.t.bookstore.core.domain.book.Book;
+import java.util.Optional;
+
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,19 +17,16 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Optional;
+import io.honeymon.springboot.t.bookstore.api.service.book.BookService;
+import io.honeymon.springboot.t.bookstore.core.domain.book.Book;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -62,10 +61,23 @@ public class BookControllerTest {
     @Test
     public void testHead() throws Exception {
         Long id = 1L;
-        when(bookService.findById(id)).thenReturn(Optional.of(new Book("test-isbn", "test-book")));
+        when(bookService.findById(id)).thenReturn(Optional.of(new Book("test-book", "test-isbn13", "test-isbn10")));
 
-        this.mockMvc.perform(head("/books/{id}", 1).contentType(MediaType.APPLICATION_JSON_UTF8))
+        this.mockMvc.perform(head("/books/{id}", id).contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isNoContent())
                 .andDo(document("books-head"));
+    }
+    
+    @Test
+    public void testGet() throws Exception {
+        Long id = 1L;
+        when(bookService.findById(id)).thenReturn(Optional.of(new Book("test-book", "test-isbn13", "test-isbn10")));
+
+        this.mockMvc.perform(get("/books/{id}", id).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.name", Is.is("test-book")))
+                .andExpect(jsonPath("$.data.isbn13", Is.is("test-isbn13")))
+                .andExpect(jsonPath("$.data.isbn10", Is.is("test-isbn10")))
+                .andDo(document("books-get"));
     }
 }
--- core/src/main/java/io/honeymon/springboot/t/bookstore/core/domain/book/Book.java
+++ core/src/main/java/io/honeymon/springboot/t/bookstore/core/domain/book/Book.java
@@ -6,30 +6,36 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.AbstractAuditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor(access=AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Book extends AbstractAuditable<Admin, Long>{
 	/**
 	 * @see <a href="http://bit.ly/2CC85aK">ISBN: International Standard Book Number</a>
 	 */
-	@Column(length=20)
-	private String isbn;
+	@Column(length=13)
+	private String isbn13;
+	@Column(length=10)
+	private String isbn10;
 	
 	private String name;
 //	private Author author;
-//	private Publisher publiser;
+//	private Publisher publisher;
 //	private List<Review> reviews;
 //	private LocalDate published;
 
 
-	public Book(String isbn, String name) {
-		this.isbn = isbn;
+	public Book(String name, String isbn13, String isbn10) {
 		this.name = name;
+		this.isbn13 = isbn13;
+		this.isbn10 = isbn10;
 	}
 }
